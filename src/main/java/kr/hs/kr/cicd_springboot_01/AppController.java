8fa1e2aa9819088cf722aa69b8b641086bd74f16
leewoojin1234 <geonui542@gmail.com>
2026-06-23 09:38:37 +0900

    f

--- src/main/java/kr/hs/kr/cicd_springboot_01/AppController.java
+++ src/main/java/kr/hs/kr/cicd_springboot_01/AppController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
+    private int k = 1;
     @GetMapping("/")
     public String home() {
         return "Hello, World!";
